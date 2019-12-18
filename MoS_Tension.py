e1a248ea647b03b3131a3d6a27ecacbed4a92d39
dvlot <58387820+dvlot@users.noreply.github.com>
2019-12-18 15:49:30 +0100

    Add files via upload

--- MoS_Tension.py
+++ MoS_Tension.py
@@ -19,20 +19,20 @@ def Normalstress(span_position):
     width, frontheight, rearheight = initial_values(z)[2:5]
     centroidx, centroidy = centroid(z)
 
-    x1, y1 = -0.5 * width - centroidx, 0.5 * rearheight - centroidy
-    x2, y2 = 0.5 * width - centroidx, 0.5 * frontheight - centroidy
+    #x1, y1 = -0.5 * width - centroidx, 0.5 * rearheight - centroidy
+    #x2, y2 = 0.5 * width - centroidx, 0.5 * frontheight - centroidy
     x3, y3 = 0.5 * width - centroidx, -0.5 * frontheight - centroidy
     x4, y4 = -0.5 * width - centroidx, -0.5 * rearheight - centroidy
 
-    sigma1 = ((M_x * I_yy - M_y * I_xy) * y1 + (M_y * I_xx - M_x * I_xy) * x1) / (I_xx * I_yy - I_xy ** 2)
-    sigma2 = ((M_x * I_yy - M_y * I_xy) * y2 + (M_y * I_xx - M_x * I_xy) * x2) / (I_xx * I_yy - I_xy ** 2)
+    #sigma1 = ((M_x * I_yy - M_y * I_xy) * y1 + (M_y * I_xx - M_x * I_xy) * x1) / (I_xx * I_yy - I_xy ** 2)
+    #sigma2 = ((M_x * I_yy - M_y * I_xy) * y2 + (M_y * I_xx - M_x * I_xy) * x2) / (I_xx * I_yy - I_xy ** 2)
     sigma3 = ((M_x * I_yy - M_y * I_xy) * y3 + (M_y * I_xx - M_x * I_xy) * x3) / (I_xx * I_yy - I_xy ** 2)
     sigma4 = ((M_x * I_yy - M_y * I_xy) * y4 + (M_y * I_xx - M_x * I_xy) * x4) / (I_xx * I_yy - I_xy ** 2)
 
-    return -sigma1, -sigma2, -sigma3, -sigma4
+    return -sigma3, -sigma4#, -sigma3, -sigma4
 
 b = 27.14
-dz = 0.1
+dz = 1
 z = 0.1
 
 sigma1tab, sigma2tab, sigma3tab, sigma4tab = [], [], [], []
@@ -40,40 +40,27 @@ alphatab, ztab, M_xtab, M_ytab = [], [], [], []
 I_xxtab, I_yytab, I_xytab = [], [], []
 MoS_Tension = []
 while z<= b:
-    sigma1, sigma2, sigma3, sigma4 = Normalstress(z)
-    M_x, M_y = internal_moments(z)
-    I_xx, I_yy, I_xy = MOI(z)
-
-    
-    
-#    if max(sigma1,sigma2, sigma3, sigma4) > 15*10**6:
+    sigma3, sigma4  = Normalstress(z)
+    #M_x, M_y = internal_moments(z)
+    #I_xx, I_yy, I_xy = MOI(z)
     ztab.append(z)
-    alphatab.append(NAangle(z)) 
-    MoS_Tension.append(1/(414/(max(sigma1,sigma2, sigma3, sigma4)/10**6)))
-    #sigma2tab.append(414/(sigma2/10**6))
-    #sigma3tab.append(414/(sigma3/10**6))
-    #sigma4tab.append(414/(sigma4/10**6))
-    M_xtab.append(M_x)
-    M_ytab.append(M_y)
-    I_xxtab.append(I_xx)
-    I_yytab.append(I_yy)
-    I_xytab.append(I_xy)
+    #alphatab.append(NAangle(z)) 
+    MoS_Tension.append(1/(414/(max(sigma3,sigma4)/10**6)))
+    #M_xtab.append(M_x)
+    #M_ytab.append(M_y)
+    #I_xxtab.append(I_xx)
+    #I_yytab.append(I_yy)
+    #I_xytab.append(I_xy)
 
     z = z+dz
 
 plt.title('Margin of safety for tensile stress')
 plt.plot(ztab,MoS_Tension, label ='point 1')
-#plt.plot(ztab,sigma2tab, label = 'point 2')
-#plt.plot(ztab,sigma3tab, label = 'point 3')
-#plt.plot(ztab,sigma4tab, label = 'point 4')
 plt.xlabel('Spanwise position [m]')
 plt.ylabel('Margin of safety')
 plt.minorticks_on()
 plt.grid(b=True, which='major', color='#bebebe', linestyle='-')
 plt.grid(b=True, which='minor', color='#e9e9e9', linestyle='-', linewidth = 0.5)
-
-
-
 plt.show()
 
 """
