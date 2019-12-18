d36881143533105cc70aae218eb2c8c6b89e777e
dhotters <57628184+dhotters@users.noreply.github.com>
2019-12-18 17:27:31 +0100

    Add files via upload

--- Deflection.py
+++ Deflection.py
@@ -1,7 +1,11 @@
 import moment_of_inertia
 import numpy as np
+import matplotlib.pyplot as plt
+import VectorlistLiftplane
+import VectorlistDragplane
+import time
 
-def Deflection(spanAr, forceAr, halfspan = 57.443/2, E = 71*10**9):
+def Deflection(halfspan = 57.443/2, E = 71.7*10**9, xEngine1 = 0.4*57.443/2, xEngine2 = 0.7*57.443/2):
     '''
     Outputs an array of deflection values and slopes dependant on the span array and force array
     :param spanAr:
@@ -11,17 +15,23 @@ def Deflection(spanAr, forceAr, halfspan = 57.443/2, E = 71*10**9):
     :return:
     '''
 
+    wingload = VectorlistLiftplane.Liftplaneforce()
+    #wingload = VectorlistDragplane.Dragplaneforce()
+
+    spanAr = wingload[0]
+    forceAr = wingload[1]
+
+    dx = spanAr[1]-spanAr[0]
+
     # Deflection caused by point loads
-    def pointLoadDeflection(F, distForce, x, halfspan, E):
-        I = sum(moment_of_inertia.MOI(distForce))
+    def pointLoadDeflection(F, distForce, x, halfspan, E, I):
 
         if 0 <= x <= distForce:
             return (F*x**2*(3*distForce-x))/(6*E*I)
         elif distForce <= x <= halfspan:
             return (F*distForce**2*(3*x-distForce))/(6*E*I)
 
-    def pointLoadSlop(F, distForce, x, halfspan, E):
-        I = sum(moment_of_inertia.MOI(distForce))
+    def pointLoadSlop(F, distForce, x, halfspan, E, I):
 
         if 0 <= x <= distForce:
             return (F*x*(2*distForce-x))/(2*E*I)
@@ -31,22 +41,52 @@ def Deflection(spanAr, forceAr, halfspan = 57.443/2, E = 71*10**9):
 
     deflectionAr = []
     slopeAr = []
+    np.array(deflectionAr)
+    np.array(slopeAr)
+
     # Run through each point load
+    print("Starting deflection calculation")
+    start_time = time.time()
     for i in range(0, len(spanAr)):
         # Run through each spanwise location of the point load
         distribution = []
         slope = []
-        for x in range(0, len(spanAr)):
-            distribution.append(pointLoadDeflection(forceAr[i], spanAr[i], spanAr[x], halfspan, E))
-            slope.append(pointLoadSlop(forceAr[i], spanAr[i], spanAr[x], halfspan, E))
-        deflectionAr.append(distribution)
-        slopeAr.append(slope)
 
-    np.array(deflectionAr)
-    np.array(slopeAr)
+        I = moment_of_inertia.MOI(spanAr[i])[0]
+
+        for x in range(0, len(spanAr)):
+            if spanAr[i]-dx/2 <= xEngine1 <= spanAr[i]+dx/2 or  spanAr[i]-dx/2 <= xEngine2 <= spanAr[i]+dx/2:
+                distribution.append(pointLoadDeflection(forceAr[i], spanAr[i], spanAr[x], halfspan, E, I))
+                #slope.append(pointLoadSlop(forceAr[i], spanAr[i], spanAr[x], halfspan, E, I))
+            else:
+                distribution.append(pointLoadDeflection(forceAr[i], spanAr[i], spanAr[x], halfspan, E, I))
+                #slope.append(pointLoadSlop(forceAr[i], spanAr[i], spanAr[x], halfspan, E, I))
+        deflectionAr.append(distribution)
+        #slopeAr.append(slope)
+        print("Deflection progress: " + str(i+1) +"/" + str(len(spanAr)) + " | " + str(round((i+1)*100/(len(spanAr)),2)) + "%")
 
     # Add up all deflections of all point loads
     deflectionAr = np.sum(deflectionAr, axis=0)
-    slopeAr = np.sum(slopeAr, axis=0)
+    #slopeAr = np.sum(slopeAr, axis=0)
+    print("End deflection calculations, excecuting time: " + str(round(time.time()-start_time,2)) + " seconds")
+    print("Tip deflection: " + str(round(deflectionAr[-1], 3)) + "m")
+
+    plt.grid()
+    plt.subplot(311)
+    plt.title("Force and deflection vs span")
+    plt.xlabel("Span [m]")
+    plt.ylabel("Force [N]")
+    plt.plot(wingload[0], wingload[1], color='blue')
+    plt.subplot(312)
+    plt.xlabel("Span [m]")
+    plt.ylabel("Deflection [m]")
+    plt.plot(wingload[0], deflectionAr, color='red')
+    #plt.subplot(313)
+    #plt.xlabel("Span [m]")
+    #plt.ylabel("Slope")
+    #plt.plot(wingload[0], slopeAr, color='green')
+    plt.show()
 
     return deflectionAr, slopeAr
+
+Deflection()
--- ShearStress.py
+++ ShearStress.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 def ShearStress(pos_list_xz, sh_load_xz, pos_list_yz, sh_load_yz, bend_mom_xz, bend_mom_yz):
 
     shear_stress_Ar = []
+    shear_stress_min_array = []
+
+    shear_stress_Ar_FS = []
+    shear_stress_min_array_FS = []
 
     # loop through the span locations
     for i in range(len(pos_list_yz)):
@@ -26,19 +30,17 @@ def ShearStress(pos_list_xz, sh_load_xz, pos_list_yz, sh_load_yz, bend_mom_xz, b
         # Compute the integrals
         # Because there is 4 walls only iterate to 4
         # Start in top right corner
-        integrals = [0, 0]
-
         # TODO Change initial_values[4] to [3] in case you switch the lift to be negative lift
+
         #y = -initial_values[4]/2
         y = -initial_values[3] / 2
         ytab = []
         shear_flow_Ar = []
+        shear_flow_Ar_FS = []
         while y <= initial_values[3]/2:
-            #Q_x = initial_values[0]*initial_values[4]*initial_values[2]/4 + initial_values[0] * (initial_values[4]/2 - y) * (y+(initial_values[4]/2 - y)/2)
-            Q_y = -initial_values[0] * y * initial_values[2]/2
 
-            Q_x = initial_values[0] * initial_values[3] * initial_values[2] / 4 + initial_values[0] * (
-                        initial_values[3] / 2 - y) * (y + (initial_values[3] / 2 - y) / 2)
+            Q_x = initial_values[0] * initial_values[3] * initial_values[2] / 4 + initial_values[0] * (initial_values[3] / 2 - y) * (y + (initial_values[3] / 2 - y) / 2)
+            Q_y = -initial_values[0] * y * initial_values[2]/2
 
             shear_flow_lift = c1*Q_x
             shear_flow_drag = c2*Q_y
@@ -46,20 +48,72 @@ def ShearStress(pos_list_xz, sh_load_xz, pos_list_yz, sh_load_yz, bend_mom_xz, b
 
             shear_flow_Ar.append(shear_flow_bending+shear_flow_drag+shear_flow_lift)
 
-
             y += .001
             ytab.append(y)
 
-        shear_stress_Ar.append(max(shear_flow_Ar)/initial_values[0]*10**-6)
+        y = -initial_values[4] / 2
+        while y <= initial_values[4]/2:
+            Q_y = initial_values[0] * y * initial_values[2]/2
+            Q_x_FS = initial_values[0] * initial_values[4] * initial_values[2] / 4 + initial_values[0] * (
+                        initial_values[4] / 2 - y) * (y + (initial_values[4] / 2 - y) / 2)
 
-        if i%40 == 0:
-            print("Shear progress: " + str(i + 1) + "/" + str(len(pos_list_yz)) + " | " + str(
+            shear_flow_lift_FS = c1*Q_x_FS
+            shear_flow_drag = c2*Q_y
+            shear_flow_bending = bend_mom_yz[i]/(2*moment_of_inertia.local_area(cur_Span_Loc))
+            shear_flow_Ar_FS.append(shear_flow_bending+shear_flow_drag+shear_flow_lift_FS)
+
+            y += .001
+
+
+        shear_stress_Ar.append(max(shear_flow_Ar)/initial_values[0]*10**-6)
+        shear_stress_min_array.append(min(shear_flow_Ar)/initial_values[0]*10**-6)
+
+        shear_stress_Ar_FS.append(max(shear_flow_Ar_FS)/initial_values[0]*10**-6)
+        shear_stress_min_array_FS.append(min(shear_flow_Ar_FS)/initial_values[0]*10**-6)
+
+        if i%50 == 0:
+            print("Shear stress progress: " + str(i + 1) + "/" + str(len(pos_list_yz)) + " | " + str(
             round((i + 1) * 100 / (len(pos_list_yz)), 2)) + "%")
 
-    plt.plot(pos_list_yz, shear_stress_Ar)
+    plt.subplot(211)
+    plt.plot(pos_list_yz, shear_stress_Ar, label="Maximum shear stress rear spar", marker="*", markevery=50, markersize=7)
+    plt.plot(pos_list_yz, shear_stress_min_array, label="Minimum shear stress rear spar", marker="v", markevery=50, markersize=7)
     plt.grid()
+    plt.legend()
+    plt.ylabel("Shear Stress [MPa]")
+    plt.title("Shear stress distribution over the span")
+
+    plt.subplot(212)
+    plt.plot(pos_list_yz, shear_stress_Ar_FS, label="Max shear stress front spar", marker="*", markevery=50, markersize=7)
+    plt.plot(pos_list_yz, shear_stress_min_array_FS, label="Min shear stress front spar", marker="v", markevery=50, markersize=7)
+    plt.grid()
+    plt.legend()
     plt.xlabel("Span [m]")
     plt.ylabel("Shear Stress [MPa]")
-    plt.axhline(y=shear_stress_Ar[0], lw=1, ls='dashed', color='#d62728')
     plt.show()
-    return shear_stress_Ar
+
+    # Calculate the critical shear stress
+    def tCrit(k, E, t, b, v):
+        return (np.pi ** 2 * k * E * (t / b) ** 2) / (12 * (1 - v ** 2))
+
+    k = 9.5
+    E = 71.7 * 10 ** 9  # Pa
+    initial_values = moment_of_inertia.initial_values(0)
+    tCritvalFS = tCrit(k, E, initial_values[0], initial_values[3], 0.32)
+    tCritvalRS = tCrit(k, E, initial_values[0], initial_values[4], 0.32)
+    print("Tcrit FS= " + str(tCritvalFS * 10 ** -6))
+    print("Tcrit RS= " + str(tCritvalRS * 10 ** -6))
+
+    print("halfspan: " + str(pos_list_yz[-1]) + " average height: " + str(moment_of_inertia.initial_values(pos_list_yz[int(len(pos_list_yz)/2)])[3]))
+    critStress = tCrit(k, E, moment_of_inertia.initial_values(pos_list_yz[int(len(pos_list_yz)/2)])[0], moment_of_inertia.initial_values(pos_list_yz[int(len(pos_list_yz)/2)])[3], 0.32)
+    print("critical stress: " + str(critStress))
+    # Get amount of stiffiners required
+    stiffiners = 0
+    for i in range(len(pos_list_yz)):
+        aspectRat = (pos_list_yz[-1]/(stiffiners+1))/moment_of_inertia.initial_values(pos_list_yz[i])[4]    #a/b
+        if aspectRat <= 5:
+            print(str(stiffiners) + " Stiffiners required for an AR of: " + str(aspectRat))
+            break
+        stiffiners += 1
+
+    return [[shear_stress_Ar,shear_stress_min_array],[shear_stress_Ar_FS, shear_stress_min_array_FS]]
